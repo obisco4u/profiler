590f6f99ead04e45ff0a9aecbddb472d311214c8
obisco4u <54366857+obisco4u@users.noreply.github.com>
2020-06-18 13:34:43 +0100

    uploading folders

---
+++ php_includes/db_connect.php
@@ -0,0 +1,9 @@
+<?php
+$db_conx = mysqli_connect("localhost", "root", "", "profiler");
+// Evaluate the connection
+if (mysqli_connect_errno()) {
+    echo mysqli_connect_error();
+    exit();
+
+}
+?>
